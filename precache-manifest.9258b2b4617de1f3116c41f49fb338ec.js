0caa9a4645e4bbc9480772caa259ee363f52944a
Saulius <you@example.com>
2020-10-11 23:41:29 +0300

    Updates

---
+++ precache-manifest.9258b2b4617de1f3116c41f49fb338ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0836874f1d660993b5285c0ffc6a6d",
+    "url": "/React-crud-deployed/index.html"
+  },
+  {
+    "revision": "a36daf13b299fae7f9cd",
+    "url": "/React-crud-deployed/static/css/main.6b5941ef.chunk.css"
+  },
+  {
+    "revision": "c71da6843736194c89c9",
+    "url": "/React-crud-deployed/static/js/2.00c42065.chunk.js"
+  },
+  {
+    "revision": "e212399c51522f06420ac50af0213ddb",
+    "url": "/React-crud-deployed/static/js/2.00c42065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36daf13b299fae7f9cd",
+    "url": "/React-crud-deployed/static/js/main.0b30a9d9.chunk.js"
+  },
+  {
+    "revision": "bfa943c043c0233b80ae",
+    "url": "/React-crud-deployed/static/js/runtime-main.ff82630f.js"
+  }
+]);
